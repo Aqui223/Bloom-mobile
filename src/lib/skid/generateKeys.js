90772cb9eea76f4ea1c29ad97c4d24b3a60b53a3
pip64 <76626915+pip64@users.noreply.github.com>
2025-11-04 18:18:25 +0700

    Improved SKID 1.0. moved from p-256 curve to x448

--- src/lib/skid/generateKeys.js
+++ src/lib/skid/generateKeys.js
@@ -4,21 +4,17 @@ import { sha256 } from '@noble/hashes/sha2.js';
 import { sha512 } from '@noble/hashes/sha2.js';
 
 import { ml_kem768 } from "@noble/post-quantum/ml-kem";
-import * as secp from '@noble/secp256k1';
+import { x448 } from "@noble/curves/ed448";
 import * as ed from "@noble/ed25519";
 
 import bytesToBase64 from './modules/utils/bytesToBase64';
 
-secp.hashes.hmacSha256 = (key, msg) => hmac(sha256, key, msg);
-secp.hashes.sha256 = sha256;
-secp.hashes.hmacSha256Async = async (key, msg) => hmac(sha256, key, msg);
-secp.hashes.sha256Async = async (msg) => sha256(msg);
 ed.hashes.sha512 = sha512;
 ed.hashes.sha512Async = (m) => Promise.resolve(sha512(m));
 
 export default function () {
     const { publicKey: kyberPublicKey, secretKey: kyberSecretKey } = ml_kem768.keygen();
-    const { publicKey: ecdhPublicKey, secretKey: ecdhSecretKey } = secp.keygen();
+    const { publicKey: ecdhPublicKey, secretKey: ecdhSecretKey } = x448.keygen();
     const { publicKey: edPublicKey, secretKey: edSecretKey } = ed.keygen()
 
     return { 
--- src/lib/skid/modules/crypto/ecdh/deriveECDHSecret.js
+++ src/lib/skid/modules/crypto/ecdh/deriveECDHSecret.js
@@ -2,14 +2,9 @@ import 'react-native-get-random-values';
 import { hmac } from '@noble/hashes/hmac.js';
 import { sha256 } from '@noble/hashes/sha2.js';
 
-import * as secp from "@noble/secp256k1";
-
-secp.hashes.hmacSha256 = (key, msg) => hmac(sha256, key, msg);
-secp.hashes.sha256 = sha256;
-secp.hashes.hmacSha256Async = async (key, msg) => hmac(sha256, key, msg);
-secp.hashes.sha256Async = async (msg) => sha256(msg);
+import { x448 } from "@noble/curves/ed448";
 
 export default function deriveECDHSecret(privateKey, publicKey) {
-  const fullSharedSecret = secp.getSharedSecret(privateKey, publicKey, true);
+  const fullSharedSecret = x448.getSharedSecret(privateKey, publicKey, true);
   return fullSharedSecret.slice(1);
 }
